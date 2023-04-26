bb0ce07f910afc504264cfa797a8e0bbdcfb7083
marynanesterenko <marynanesterenko95@gmail.com>
2023-04-26 08:22:41 -0400

    ChromeDriver is set up

--- src/test/java/utils/DriverUtils.java
+++ src/test/java/utils/DriverUtils.java
@@ -1,12 +1,23 @@
 package utils;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.concurrent.TimeUnit;
 
 public class DriverUtils {
 
     static WebDriver driver;
 
     public static void createDriver(){
-
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get(ConfigReader.getProperty("app.baseurl"));
     }
 }
